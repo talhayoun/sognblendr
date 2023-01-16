348c07121595e563db35cf350a3693af2474e201
Tal <talhayoun8@gmail.com>
2023-01-16 19:18:54 +0200

    changed url

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,8 +3,8 @@ import Axios from "axios";
 export const IS_PRIVATE = 'IS_PRIVATE';
 
 export const axiosInstace = Axios.create({
-    // baseURL: "http://songblendr.com/web_service/api/"
-    baseURL: "http://localhost:4000",
+    baseURL: "https://songblendr.onrender.com"
+    // baseURL: "http://localhost:4000",
 });
 
 axiosInstace.interceptors.request.use((config) => {
